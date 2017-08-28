e0dbe8e90db781f91986a20088e10c308ef2275c
skipole <42965323+bernie-skipole@users.noreply.github.com>
2017-08-28 22:40:45 +0100

    version 0.0.3, setting inputs

--- projectcode/sensors/__init__.py
+++ projectcode/sensors/__init__.py
@@ -1,34 +1,46 @@
 
-import random, collections
+import collections
 
 from ... import FailPage, GoTo, ValidateError, ServerError
-
-_SENSORS = ["one", "two", "three"]
+from .. import hardware
 
 
 def sensor_table01(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
     """sets two lists for sensor table 01 into page data"""
-    page_data['table01', 'col1'] = _SENSORS
-    # and set values
+    page_data['table01', 'col1'] = hardware.get_input_names()
+    # and set values into col2
     sensor_table01_values(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang)
 
 
 def sensor_table01_values(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
     """sets values list for sensor table 01 into page data"""
-    page_data['table01', 'col2'] = _get_sensor_values()
+    sensors = hardware.get_input_names()
+    page_data['table01', 'col2'] = _get_sensor_values(sensors)
 
 
 def sensors_json_api(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
     "Returns sensors dictionary"
-    sensors = _SENSORS
-    values = _get_sensor_values()
+    sensors = hardware.get_input_names()
+    values = _get_sensor_values(sensors)
     return collections.OrderedDict(zip(sensors,values))
 
 
-def _get_sensor_values():
+def _get_sensor_values(sensors):
     "Returns list of sensor values"
-    # currently just sets three random floating point numbers
-    n1 = str(round(random.random(),4))
-    n2 = str(round(random.random(),4))
-    n3 = str(round(random.random(),4))
-    return [ n1, n2, n3  ]
+    values = []
+    for name in sensors:
+        input_type = hardware.get_input_type(name)
+        if input_type == 'boolean':
+            invalue = hardware.get_boolean_input(name)
+            if invalue is None:
+                value = 'UNKNOWN'
+            elif invalue:
+                value = 'ON'
+            else:
+                value = 'OFF'
+        elif input_type == 'text':
+            value = hardware.get_text_input(name)
+        else:
+            value = ''
+        values.append(value)
+    return values
