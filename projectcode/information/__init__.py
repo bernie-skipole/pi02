33f8a58bec467bf590b4e4ec7f15128d43328b2b
skipole <bernie@skipole.co.uk>
2018-09-02 16:05:25 +0100

    Removed unused information folder and contents

--- projectcode/information/__init__.py
+++
@@ -1,8 +0,0 @@
-from ... import FailPage, GoTo, ValidateError, ServerError
-
-
-def information_page(caller_ident, ident_list, submit_list, submit_dict, call_data, page_data, lang):
-    """Set up the information page"""
-    if ('HTTP_HOST' in call_data) and call_data['HTTP_HOST']:
-        page_data['control_json', 'content'] = call_data['HTTP_HOST'] + "/controls.json"
-        page_data['sensors_json', 'content'] = call_data['HTTP_HOST'] + "/sensors.json"
